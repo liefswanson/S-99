ed002eb0701d4fceb50a6cdc3c10c6fdce876b90
liefswanson <liefs@sfu.ca>
2015-07-28 12:17:52 -0700

    P18 modified.

--- src/main/scala/18.scala
+++ src/main/scala/18.scala
@@ -1,7 +1,7 @@
 object P18 {
   def sliceCheating[A](a:Int, b:Int, ls:List[A]):List[A] = ls drop a take b-(a max 0)
 
-  def slice[A](a:Int, b:Int, ls:List[A]):List[A] = {
+  def sliceUgly[A](a:Int, b:Int, ls:List[A]):List[A] = {
     def take(n:Int, ls:List[A]):List[A] = (n, ls) match {
       case (_, Nil) => Nil
       case (0, _) => Nil
@@ -13,5 +13,12 @@ object P18 {
       case (i, h::tail) => drop(i-1, tail)
     }
     take(b-a, drop(a, ls))
+  }
+
+  def slice[A](a:Int, b:Int, ls:List[A]):List[A] = (a, b, ls) match {
+    case (_, _, Nil)     => Nil
+    case (_, 0, _)       => Nil
+    case (0, e, h::tail) => h::slice(0,   e-1, tail)
+    case (s, e, h::tail) =>    slice(s-1, e-1, tail)
   }
 }
