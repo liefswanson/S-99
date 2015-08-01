3f3f6895a564498a7644a098ce9cdb3cade75ca9
liefs <liefs@sfu.ca>
2015-08-01 14:21:37 -0700

    P22 add step option

--- src/main/scala/22.scala
+++ src/main/scala/22.scala
@@ -10,4 +10,20 @@ object P22 {
     }
     rangeHelper(a, b, Nil)
   }
+
+  // with step!
+  def range(a:Int, b:Int, step:Int):List[Int] = {
+    val iterate =
+      if (a<b) (x:Int) => x+step
+      else     (x:Int) => x-step
+
+    def rangeHelper(a:Int, b:Int, ls:List[Int]):List[Int] = b-a match {
+      case n if n.abs < step => (a::ls).reverse
+      case _                 => rangeHelper(iterate(a), b, a::ls)
+    }
+    rangeHelper(a, b, Nil)
+  }
+
+  def range2(a:Int, b:Int):List[Int] = a to b toList
+  def range2(a:Int, b:Int, step:Int):List[Int] = a to b by step toList
 }
