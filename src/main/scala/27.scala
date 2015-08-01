cc7a58d408584705fadbde8b71d5691c48fb8c8b
liefs <liefs@sfu.ca>
2015-08-01 14:01:40 -0700

    P27 add diff function to substitute --

--- src/main/scala/27.scala
+++ src/main/scala/27.scala
@@ -1,12 +1,14 @@
 object P27 {
   import P26.combinations
 
+  def diff[A](ls: List[A], ls2: List[A]): List[A] = ls.filterNot(ls2.toSet) 
+
   def group3[A](ls: List[A]): List[List[List[A]]] =
     for {
       a <- combinations(2, ls)
-      noA = ls.filterNot(a.toSet)
+      noA = diff(ls, a)
       b <- combinations(3, noA)
-    } yield List(a, b, noA.filterNot(b.toSet))
+    } yield List(a, b, diff(noA, b))
 
   def group[A](ns: List[Int], ls: List[A]): List[List[List[A]]] = ns match {
     case Nil     => List(Nil)
